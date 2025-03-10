2018dda22893ee19217dee2327f3f2f3ba956977
Jevon Putra <jevonop88@gmail.com>
2025-03-10 13:31:11 +0700

    Add local databse for store all surah

--- app/src/main/java/com/jop/ngaji/data/local/room/converter/ConverterAudioFull.kt
+++ app/src/main/java/com/jop/ngaji/data/local/room/converter/ConverterAudioFull.kt
@@ -1,21 +1,21 @@
-package com.jop.ngaji.room.converter
+package com.jop.ngaji.data.local.room.converter
 
 import androidx.room.TypeConverter
-import com.jop.ngaji.data.model.AudioFull
+import com.jop.ngaji.data.model.Surah
 import kotlinx.serialization.json.Json
 
 class ConverterAudioFull {
     companion object{
         @TypeConverter
         @JvmStatic
-        fun fromVariants(value: AudioFull): String {
+        fun fromVariants(value: Surah.Ayah.AudioFull): String {
             return Json.encodeToString(value)
         }
 
         @TypeConverter
         @JvmStatic
-        fun toVariants(value: String): AudioFull {
-            return Json.decodeFromString<AudioFull>(value)
+        fun toVariants(value: String): Surah.Ayah.AudioFull {
+            return Json.decodeFromString<Surah.Ayah.AudioFull>(value)
         }
     }
 }
