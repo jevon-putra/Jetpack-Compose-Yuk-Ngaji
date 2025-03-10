424bb9ef329333a2c1ad9a78184521a262fb5ba1
Jevon Putra <jevonop88@gmail.com>
2025-03-10 13:36:22 +0700

    Dependency injection updates: - Add `PrayRepository` and `PrayAPI`. - Add `DataStoreSetting`. - Add `HomeViewModel`. - Update `DetailSurahViewModel`. - Replace `ExoPlayer` with `ExoPlayerHelper`. - Update `SurahAPI` and move network configuration to `NetworkClient`.

--- app/src/main/java/com/jop/ngaji/di/ViewModelsModule.kt
+++ app/src/main/java/com/jop/ngaji/di/ViewModelsModule.kt
@@ -1,11 +1,21 @@
 package com.jop.ngaji.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.jop.ngaji.data.local.store.DataStoreSetting
 import com.jop.ngaji.presentation.detailSurah.viewModel.DetailSurahViewModel
+import com.jop.ngaji.presentation.home.viewModel.HomeViewModel
 import com.jop.ngaji.presentation.surah.viewModel.SurahViewModel
+import com.jop.ngaji.util.dataStore
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelsModule = module {
+    single<DataStore<Preferences>>{ get<Context>().dataStore }
+    single<DataStoreSetting> { DataStoreSetting(get()) }
+
     viewModel<SurahViewModel>{ SurahViewModel(get()) }
-    viewModel<DetailSurahViewModel>{ DetailSurahViewModel(get(), get()) }
+    viewModel<HomeViewModel>{ HomeViewModel(get(), get(), get()) }
+    viewModel<DetailSurahViewModel>{ DetailSurahViewModel(get(), get(), get()) }
 }
